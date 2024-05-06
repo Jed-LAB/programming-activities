4fc0b55dca42848cf60d113fa74308d39b717250
Jed-ivan (Jed-LAB) <136905749+Jed-LAB@users.noreply.github.com>
2024-05-06 22:20:11 +0800

    PrintTheNumber.c

---
+++ PrintTheNumber.c
@@ -0,0 +1,16 @@
+/////////////////////////////
+// created by : jed lab
+// purpose to print 1 to 10
+// Write a program that prints all the numbers from 1 to 10 (or any other range you specify) using a while loop.
+// date created: May 06, 2024 / 10:20 pm GMT+8
+  
+#include<stdio.h>
+int main(){
+int ptNum = 1;
+
+while (ptNum < 11){
+      printf("%d\n", ptNum);
+      ptNum++;
+    }
+    return 0;
+}
